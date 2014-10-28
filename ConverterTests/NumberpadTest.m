2d3dde8feaa6d435c7c89b848ef5ea54f100cd47
Holger Bohlmann <hbo@it-agile.de>
2014-10-28 21:58:48 +0100

    Breaking the fest

--- ConverterTests/NumberpadTest.m
+++ ConverterTests/NumberpadTest.m
@@ -20,7 +20,7 @@
 
 -(void) testInputOneNumber {
 	[pad numberTouched:3];
-	assertThat([pad displayValue], equalTo(@"3"));
+	assertThat([pad displayValue], equalTo(@"2"));
 }
 
 -(void) testSecondNumberAddsOnFirstnumber {
