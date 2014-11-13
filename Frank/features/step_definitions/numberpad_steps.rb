d2c35549a4a63a91ec2c9f941fdf4639e9a36016
Sven Günther <sven.guenther@it-agile.de>
2014-11-13 10:12:05 +0100

    remove frank

--- Frank/features/step_definitions/numberpad_steps.rb
+++
@@ -1,9 +0,0 @@
-When(/^I enter "(.*?)"$/) do |value|
-	value.chars.each do |chr|
-		touch("button marked:'#{chr}'")
-	end
-end
-
-Then /^I save a screenshot named "(.*?)"$/ do |filename|
- frankly_screenshot(filename+'.png')
-end
